cb7b19d995fbeee8973327c818f7a6fef3717430
GtOliv3r <123522878+GtOliv3r@users.noreply.github.com>
2024-03-28 22:43:14 -0300

    feat:models
    added models.py file

--- api/models.py
+++ api/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+class ClassEntity:
+    
+    def __init__(self, nome, responsael, id) -> None:
+        self.id = id
+        self.nome = nome
+        self.responsavel = responsael
+        
+
+        
+    def __str__(self) -> str:
+        return (f"Class <{self.temperature}>")
+    
+    def __getattribute__(self, __name: str) -> any:
+        if (__name=='date'):
+            return object.__getattribute__(self, __name).strftime("%d/%m/%Y %H:%M:%S")
+        else:
+            return object.__getattribute__(self, __name)
